78a1b475bf364fd2ae43788db3d23de453bd266b
ArtRomanWorx <artromanworx@users.noreply.github.com>
2020-04-16 05:42:44 +0300

    Fix Debuger

--- includes/vendor/debuger.php
+++ includes/vendor/debuger.php
@@ -3,74 +3,74 @@
     <div class="memoryUsage debugBlock">
         <span class="title">Base URL</span>
         <?php
-            echo '<i class="fas fa-link"></i> ' . base_url();
+        echo '<i class="fas fa-link"></i> ' . base_url();
         ?>
     </div>
     <div class="memoryUsage debugBlock">
         <span class="title">Root Directory</span>
         <?php
-            echo '<i class="fas fa-folder"></i> ' . $_SERVER['DOCUMENT_ROOT'];
+        echo '<i class="fas fa-folder"></i> ' . $_SERVER['DOCUMENT_ROOT'];
         ?>
     </div>
     <div class="memoryUsage debugBlock">
         <span class="title">Current Working Directory</span>
         <?php
-            echo '<i class="fas fa-folder-open"></i> ' . getcwd();
+        echo '<i class="fas fa-folder-open"></i> ' . getcwd();
         ?>
     </div>
     <div class="memoryUsage debugBlock">
         <span class="title">Server IP</span>
         <?php
-            echo '<i class="fas fa-globe"></i> ' . get_client_ip_server();
+        echo '<i class="fas fa-globe"></i> ' . gethostbyname('www.' . str_replace(['/', ':', 'http', 'https'], '', base_url()));
         ?>
     </div>
     <div class="memoryUsage debugBlock">
         <span class="title">Your IP</span>
         <?php
-            echo '<i class="fas fa-globe"></i> ' . get_client_ip_env();
+        echo '<i class="fas fa-globe"></i> ' . $_SERVER['REMOTE_ADDR'];
         ?>
     </div>
     <div class="memoryUsage debugBlock">
         <span class="title">OS Family</span>
         <?php
-            if (PHP_OS_FAMILY === 'Windows') {
-                echo '<i class="fab fa-windows"></i> ' . PHP_OS_FAMILY;
-            }
-            if (PHP_OS_FAMILY === 'MacOS') {
-                echo '<i class="fab fa-apple"></i> ' . PHP_OS_FAMILY;
-            }
-            if (PHP_OS_FAMILY === 'Linux') {
-                echo '<i class="fab fa-linux"></i> ' . PHP_OS_FAMILY;
-            } if (PHP_OS_FAMILY === '') {
-                echo 'Unknown operating system';
-            }
+        if (PHP_OS_FAMILY === 'Windows') {
+            echo '<i class="fab fa-windows"></i> ' . PHP_OS_FAMILY;
+        }
+        if (PHP_OS_FAMILY === 'MacOS') {
+            echo '<i class="fab fa-apple"></i> ' . PHP_OS_FAMILY;
+        }
+        if (PHP_OS_FAMILY === 'Linux') {
+            echo '<i class="fab fa-linux"></i> ' . PHP_OS_FAMILY;
+        } if (PHP_OS_FAMILY === '') {
+            echo 'Unknown operating system';
+        }
         ?>
     </div>
     <div class="memoryUsage debugBlock">
         <span class="title">Server</span>
         <?php
-            echo '<i class="fas fa-server"></i> ' . $_SERVER["SERVER_SOFTWARE"];
+        echo '<i class="fas fa-server"></i> ' . $_SERVER["SERVER_SOFTWARE"];
         ?>
     </div>
     <div class="memoryUsage debugBlock">
         <span class="title">PHP Version</span>
         <?php
-            echo '<i class="fab fa-php"></i> ' . phpversion();
+        echo '<i class="fab fa-php"></i> ' . phpversion();
         ?>
     </div>
     <div class="memoryUsage debugBlock" data-has-sub="true">
         <span class="title">Tables</span>
         <?php
-            $listOfTables = R::inspect();
-            //var_dump($listOfTables);
-            echo '<i class="fas fa-database"></i> ' . count($listOfTables);
+        $listOfTables = R::inspect();
+        //var_dump($listOfTables);
+        echo '<i class="fas fa-database"></i> ' . count($listOfTables);
         ?>
         <div class="dbSub">
             <ul>
                 <?php
-                    foreach($listOfTables as $listOfTable) {
-                        echo '<li>' . $listOfTable . '</li>';
-                    }
+                foreach($listOfTables as $listOfTable) {
+                    echo '<li>' . $listOfTable . '</li>';
+                }
                 ?>
             </ul>
         </div>
@@ -82,17 +82,17 @@
     <div class="memoryUsage debugBlock">
         <span class="title">RAM Limit</span>
         <?php
-            $memory_limit = ini_get('memory_limit');
-            echo '<i class="fas fa-memory"></i> ' . $memory_limit;
+        $memory_limit = ini_get('memory_limit');
+        echo '<i class="fas fa-memory"></i> ' . $memory_limit;
         ?>
     </div>
     <div class="memoryUsage debugBlock">
         <span class="title">RAM Usage</span>
         <?php
-            $bytes = (memory_get_usage() - $mem_start);
-            $kbytes = ((memory_get_usage() - $mem_start) / 1000);
-            $mbytes = ((memory_get_usage() - $mem_start) / 1000000);
-            echo '<i class="fas fa-memory"></i> ' . round($mbytes, 2) . 'M';
+        $bytes = (memory_get_usage() - $mem_start);
+        $kbytes = ((memory_get_usage() - $mem_start) / 1000);
+        $mbytes = ((memory_get_usage() - $mem_start) / 1000000);
+        echo '<i class="fas fa-memory"></i> ' . round($mbytes, 2) . 'M';
         ?>
     </div>
 </div>
